89a011b8a6c7fa6d7d3a0beb47cd8ed896dfc4fa
Dual Phoenix <nqkhang@apcs.vn>
2018-08-25 00:15:24 +0200

    initial commit

---
+++ source_code_in_C_plus_plus.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    
+    while (x == y) {
+        something();
+        somethingelse();
+
+        if (some_error)
+            do_correct();
+        else
+            continue_as_usual();
+    }
+
+    finalthing();
+    
+}
+
